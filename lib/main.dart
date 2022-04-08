aafd602fb8704dccdc9bbdb6c797cc63f4603a72
MaryemJaouadii <mariamjawadi271@gmail.com>
2022-04-08 06:34:21 +0100

    fixing all problems

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,139 @@
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:foodproject/screens/HomeScreen.dart';
+// import 'package:foodproject/screens/LoginTab.dart';
+// import 'package:foodproject/screens/RegisterTab.dart';
+// import 'package:foodproject/screens/cameraDetection.dart';
+//
+// import 'package:camera/camera.dart';
+//
+// List<CameraDescription> cameras={} as List<CameraDescription>;
+// Future<void> main() async {
+//
+//
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   cameras= await availableCameras();
+//   runApp(HomePage());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: CameraDetection(),
+//       theme: ThemeData(
+//           brightness: Brightness.light,
+//         fontFamily: 'Poppins'
+//       ),
+//     );
+//   }
+// }
+//
+//
+//
+//
+//
+//
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Timer(Duration(seconds: 3), () {
+//       Navigator.of(context)
+//           .pushReplacement(MaterialPageRoute(builder: (_) => HomePage()));
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//
+//     return Scaffold(
+//
+//         body: Stack(
+//           children: [
+//                 Image.asset(
+//                   'images/splashscreen.png',
+//                   width: MediaQuery.of(context).size.width,
+//
+//                 ),
+//             Padding(
+//               padding: const EdgeInsets.only(top:600.0),
+//               child: Text("Let's cook your own food and adjust your diet!", style: TextStyle(fontSize: 25), textAlign: TextAlign.center,),
+//             ),
+//
+//
+//           ],
+//         ));
+//
+//
+//
+//
+//   }
+// }
+//
+//
+//
+//
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SplashScreen(),
+//       theme: ThemeData(
+//
+//           brightness: Brightness.light,
+//       ),
+//       initialRoute: LoginTab.id,
+//
+//       routes: {
+//         CameraDetection.id : (context) => CameraDetection(),
+//         LoginTab.id: (context)=>LoginTab(),
+//         RegisterTab.id: (context)=>RegisterTab(),
+//         HomeScreen.id: (context)=>HomeScreen(),
+//
+//
+//
+//
+//
+//
+//       },
+//     );
+//   }
+// }
+//
+
+
+
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:foodproject/screens/HomeScreen.dart';
 import 'package:foodproject/screens/LoginTab.dart';
 import 'package:foodproject/screens/RegisterTab.dart';
-import 'package:foodproject/screens/cameraDetection.dart';
 
-import 'package:camera/camera.dart';
 
-List<CameraDescription> cameras={} as List<CameraDescription>;
-Future<void> main() async {
+
+
+void main() async {
 
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  cameras= await availableCameras();
+
   runApp(MyApp());
 }
 
@@ -22,10 +143,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CameraDetection(),
+      home: SplashScreen(),
       theme: ThemeData(
           brightness: Brightness.light,
-        fontFamily: 'Poppins'
+          fontFamily: 'Poppins'
       ),
     );
   }
@@ -59,11 +180,11 @@ class _SplashScreenState extends State<SplashScreen> {
 
         body: Stack(
           children: [
-                Image.asset(
-                  'images/splashscreen.png',
-                  width: MediaQuery.of(context).size.width,
+            Image.asset(
+              'images/splashscreen.png',
+              width: MediaQuery.of(context).size.width,
 
-                ),
+            ),
             Padding(
               padding: const EdgeInsets.only(top:600.0),
               child: Text("Let's cook your own food and adjust your diet!", style: TextStyle(fontSize: 25), textAlign: TextAlign.center,),
@@ -95,14 +216,15 @@ class _HomePageState extends State<HomePage> {
       home: SplashScreen(),
       theme: ThemeData(
 
-          brightness: Brightness.light,
+        brightness: Brightness.light,
       ),
       initialRoute: LoginTab.id,
 
       routes: {
-        CameraDetection.id : (context) => CameraDetection(),
         LoginTab.id: (context)=>LoginTab(),
         RegisterTab.id: (context)=>RegisterTab(),
+        HomeScreen.id: (context)=>HomeScreen(),
+
 
 
 
