09acb1968cc723a6ff926c66a9635f90d4c6f785
MaryemJaouadii <mariamjawadi271@gmail.com>
2022-04-08 06:22:31 +0100

    fix pubspec yaml file

--- lib/Models/IngredientCategory.dart
+++ lib/Models/IngredientCategory.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodproject/Models/Ingredient.dart';
-import 'package:multi_select_flutter/multi_select_flutter.dart';
+
 
 
 List categories = [
