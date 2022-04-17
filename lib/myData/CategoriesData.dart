191b4aaf8f0cde1ffecabbcd49f8829f0476f2d3
Shayma TRAD <54177715+Nouha-Smairi@users.noreply.github.com>
2022-04-17 17:34:41 +0200

    add Oils Category

--- lib/myData/CategoriesData.dart
+++ lib/myData/CategoriesData.dart
@@ -1,11 +1,11 @@
 import '../Models/Category.dart';
 
-  List categoriesData = [
-    Category("Dp", "images/dairy2.jpg", "Eggs and Diary products"),
-    Category("Veg", "images/vegetables.jpg", "Vegetables"),
-    Category("Spc", "images/spices.jpg", "Spices, herbs and seeds"),
-    Category("Sg", "images/sugar.jpg", "Sugar and chocolates"),
-    Category("Fr", "images/fruit.jpg", "Fruits"),
-    Category("Fl", "images/flour.jpg", "Flours and yeasts"),
-   // Category("Oil", "images/oil.jpg", "Oils and fats")
-  ];
+List categoriesData = [
+  Category("Dp", "images/dairy2.jpg", "Eggs and Diary products"),
+  Category("Veg", "images/vegetables.jpg", "Vegetables"),
+  Category("Spc", "images/spices.jpg", "Spices, herbs and seeds"),
+  Category("Sg", "images/sugar.jpg", "Sugar and chocolates"),
+  Category("Fr", "images/fruit.jpg", "Fruits"),
+  Category("Fl", "images/flour.jpg", "Flours and yeasts"),
+  Category("Oil", "images/oil.jpg", "Oils and fats")
+];
